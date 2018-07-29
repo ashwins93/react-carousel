fa4e6e4b4b0e2131a34281b214e6e6176f5ea988
Ashwin <aswinjeganath@gmail.com>
2018-07-30 04:14:09 +0530

    Add GSAP animaitons

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
-import img1 from './images/computer.jpeg';
-import img2 from './images/mountains.jpeg';
-import img3 from './images/trees.jpeg';
-import img4 from './images/turntable.jpeg';
+import img1 from './images/image1.jpg';
+import img2 from './images/image2.jpg';
+import img3 from './images/image3.jpg';
+import img4 from './images/image4.jpg';
 import Carousel from './components/Carousel';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Carousel images={[img1, img2, img3, img4]} />
+        <Carousel height="300px" images={[img1, img2, img3, img4]} />
       </div>
     );
   }
--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,35 +1,100 @@
 import React from 'react';
+import TweenMax from 'gsap/TweenMax';
 import './Carousel.css';
 
 class Carousel extends React.Component {
   state = {
     images: [...this.props.images],
-    currentImage: 0
+    animating: false,
+    currentImage: 0,
+    incomingImage: 1
   };
 
+  currentImgRef = React.createRef();
+  incomingImgRef = React.createRef();
+
   next = () => {
-    this.setState(prevState => ({
-      currentImage: (prevState.currentImage + 1) % prevState.images.length
-    }));
+    let incoming = (this.state.currentImage + 1) % this.state.images.length;
+    this.setState(
+      prevState => ({
+        incomingImage: incoming,
+        animating: true
+      }),
+      () => {
+        TweenMax.to(this.currentImgRef.current, 0.5, {
+          xPercent: -100,
+          onComplete: () => {
+            TweenMax.set(this.currentImgRef.current, { xPercent: 0 });
+          }
+        });
+        TweenMax.fromTo(
+          this.incomingImgRef.current,
+          0.5,
+          { xPercent: 100 },
+          {
+            xPercent: 0,
+            onComplete: () =>
+              this.setState({
+                currentImage: incoming,
+                animating: false
+              })
+          }
+        );
+      }
+    );
   };
 
   previous = () => {
-    this.setState(prevState => ({
-      currentImage:
-        prevState.currentImage - 1 >= 0
-          ? prevState.currentImage - 1
-          : prevState.images.length - 1
-    }));
+    let incoming =
+      this.state.currentImage - 1 >= 0
+        ? this.state.currentImage - 1
+        : this.state.images.length - 1;
+    this.setState(
+      prevState => ({
+        incomingImage: incoming,
+        animating: true
+      }),
+      () => {
+        TweenMax.to(this.currentImgRef.current, 0.5, {
+          xPercent: 100,
+          onComplete: () => {
+            TweenMax.set(this.currentImgRef.current, { xPercent: 0 });
+          }
+        });
+        TweenMax.fromTo(
+          this.incomingImgRef.current,
+          0.5,
+          { xPercent: -100 },
+          {
+            xPercent: 0,
+            onComplete: () =>
+              this.setState({
+                currentImage: incoming,
+                animating: false
+              })
+          }
+        );
+      }
+    );
   };
 
   render() {
     return (
-      <div className="Carousel">
+      <div className="Carousel" style={{ height: this.props.height }}>
         <img
+          ref={this.currentImgRef}
           className="Carousel__img"
           src={this.state.images[this.state.currentImage]}
           alt=""
         />
+        {this.state.animating ? (
+          <img
+            ref={this.incomingImgRef}
+            className="Carousel__img Carousel__img--animate"
+            src={this.state.images[this.state.incomingImage]}
+            alt=""
+          />
+        ) : null}
         <button
           onClick={this.previous}
           className="Carousel__button Carousel__button--left"
