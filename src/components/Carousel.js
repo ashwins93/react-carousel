79b93b8f6c306520c18f9ef528e5cb2f3a3199a4
Ashwin <aswinjeganath@gmail.com>
2018-07-30 02:47:05 +0530

    Implement carousel function

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,12 +1,47 @@
 import React from 'react';
+import './Carousel.css';
 
 class Carousel extends React.Component {
+  state = {
+    images: [...this.props.images],
+    currentImage: 0
+  };
+
+  next = () => {
+    this.setState(prevState => ({
+      currentImage: (prevState.currentImage + 1) % prevState.images.length
+    }));
+  };
+
+  previous = () => {
+    this.setState(prevState => ({
+      currentImage:
+        prevState.currentImage - 1 >= 0
+          ? prevState.currentImage - 1
+          : prevState.images.length - 1
+    }));
+  };
+
   render() {
     return (
-      <div>
-        {this.props.images.map((image, i) => {
-          return <img key={i} src={image} alt="" />;
-        })}
+      <div className="Carousel">
+        <img
+          className="Carousel__img"
+          src={this.state.images[this.state.currentImage]}
+          alt=""
+        />
+        <button
+          onClick={this.previous}
+          className="Carousel__button Carousel__button--left"
+        >
+          &larr;
+        </button>
+        <button
+          onClick={this.next}
+          className="Carousel__button Carousel__button--right"
+        >
+          &rarr;
+        </button>
       </div>
     );
   }
